63d69e652c59b042c748fae1f2e33d66d71c48ab
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2024-09-06 23:25:38 +0330

    The styling of the forms was done

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -4,6 +4,8 @@ import { v4 } from "uuid";
 import ContactsList from "./ContactsList";
 import inputs from "./constants/inputs";
 
+import styles from "./Contacts.module.css";
+
 function Contacts() {
   const [contacts, setContacts] = useState([]);
   const [alert, setAlert] = useState("");
@@ -49,8 +51,8 @@ function Contacts() {
   };
 
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.form}>
         {inputs.map((input, index) => (
           <input
             key={index}
@@ -64,7 +66,7 @@ function Contacts() {
 
         <button onClick={addHandler}>Add Contacts</button>
       </div>
-      <div>{alert && <p>{alert}</p>}</div>
+      <div className={styles.alert}>{alert && <p>{alert}</p>}</div>
       <ContactsList contacts={contacts} deleteHandler={deleteHandler} />
     </div>
   );
