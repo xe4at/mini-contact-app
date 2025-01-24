f92a9783a07bd844a32a049d8a141fb22f2648f1
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2025-01-24 17:23:05 +0330

    some change in style and jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
     <div className={styles.container}>
       <h1>Contact App</h1>
       <p>
-        <a href="#">XE4AT</a> | React.js Full Course
+        <a href="#">Ehsanatashkar.js</a> | React.js Full Course
       </p>
     </div>
   );
